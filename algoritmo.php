1a2fe6ef63420c8f1138e839612460e25aee9493
EMANUEL DA SILVA CORREA <emanuel.scorrea@senacsp.edu.br>
2018-08-16 17:29:41 -0300

    All the time

---
+++ algoritmo.php
@@ -0,0 +1,19 @@
+<?php
+    class Algoritmo {
+        private $soma;
+
+        function public equacao($this->setSoma(), $this->se;) {
+            
+        }
+
+
+
+
+        function public getSoma() {
+            return $this->soma;
+        }
+        function public setSoma($s) {
+            $this->soma = $s;
+        }
+    }
+?>
